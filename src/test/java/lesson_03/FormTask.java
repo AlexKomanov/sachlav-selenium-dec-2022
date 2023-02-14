f1da47f2d07d577128be9ef9a54759ada535335d
AlexKomanov <52656401+AlexKomanov@users.noreply.github.com>
2023-02-14 20:34:25 +0200

    Form Task commit 2

--- src/test/java/lesson_03/FormTask.java
+++ src/test/java/lesson_03/FormTask.java
@@ -8,12 +8,14 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class FormTask {
 
+    static WebDriver driver;
     public static void main(String[] args) throws InterruptedException {
 
         WebDriverManager.chromedriver().setup();
-        WebDriver driver = new ChromeDriver();
+        driver = new ChromeDriver();
         driver.manage().window().maximize();
 
+        driver.get("https://www.automation.co.il/tutorials/selenium/demo1/indexID.html");
         // Page 1
 
         String expectedErrorText = "This field is required.";
@@ -22,9 +24,21 @@ public class FormTask {
         By lastName = By.id("lastname");
         By email = By.name("email");
         By nextButton = By.cssSelector("#next");
-        By firstNameError = By.cssSelector("id=\"firstname-error\"");
+        By firstNameError = By.cssSelector("[id=\"firstname-error\"]");
         By lastNameError = By.cssSelector("[id=\"lastname-error\"]");
         By emailError = By.cssSelector("[id=\"email-error\"]");
+
+        clickElement(nextButton);
+
+        validateText(firstNameError, expectedErrorText);
+        validateText(lastNameError, expectedErrorText);
+        validateText(emailError, expectedErrorText);
+
+        typeText(firstName, "Alex");
+        typeText(lastName, "Komanov");
+        typeText(email, "ako@gmail.com");
+
+        clickElement(nextButton);
 
 
 
@@ -32,7 +46,7 @@ public class FormTask {
         driver.quit();
     }
 
-    public static void validateText(By selector, String expectedText, WebDriver driver) {
+    public static void validateText(By selector, String expectedText) {
         WebElement element = driver.findElement(selector);
         if (element.getText().equals(expectedText)) {
             System.out.println("Text is correct");
@@ -42,14 +56,14 @@ public class FormTask {
         }
     }
 
-    public static void typeText(By selector, String text, WebDriver driver) {
+    public static void typeText(By selector, String text) {
         WebElement element = driver.findElement(selector);
         element.click();
         element.clear();
         element.sendKeys(text);
     }
 
-    public static void clickElement(By selector, WebDriver driver) {
+    public static void clickElement(By selector) {
         WebElement element = driver.findElement(selector);
         element.click();
     }
